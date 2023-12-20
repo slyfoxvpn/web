784b66c39f4574669fd19fc9983dc49ac0a6802d
teenage1ove <kirillstars7@gmail.com>
2023-12-20 17:29:30 +0300

    Update burger-menu

--- context/assets/js/index.js
+++ context/assets/js/index.js
@@ -1,6 +1,7 @@
 let burgerIcon = document.querySelector('.burger-menu>i')
 burgerIcon.onclick = (event) => {
     let burgerMenuBlock = document.querySelector('.burger-menu-block')
+    document.body.classList.toggle('overflow-body')
     burgerMenuBlock.classList.toggle('hide')
     if ( burgerIcon.classList.contains('fa-bars')){    burgerIcon.classList.remove('fa-bars')
     burgerIcon.classList.toggle('fa-xmark')
